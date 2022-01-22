8fa5b0bf13e6e0299af154144d5017edb1c6522f
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-01-22 12:35:08 -0300

    [FIX] website_event_sale_session: use t-out instead of t-raw (deprecated warning)

--- website_event_sale_session/static/src/js/website_event_session_booking.js
+++ website_event_sale_session/static/src/js/website_event_session_booking.js
@@ -7,11 +7,28 @@ odoo.define("website_event_sale_session.booking", function (require) {
     "use strict";
 
     const WebsiteEventSessionBooking = require("website_event_session.booking");
+    const {Markup} = require("web.utils");
 
     WebsiteEventSessionBooking.include({
         xmlDependencies: WebsiteEventSessionBooking.prototype.xmlDependencies.concat([
             "/website_event_sale_session/static/src/xml/website_event_session_booking.xml",
         ]),
+
+        /**
+         * @override
+         */
+        async _getSessionTickets() {
+            const html_fields = ["price_html", "price_reduce_html"];
+            const tickets = await this._super.apply(this, arguments);
+            for (const ticket of tickets) {
+                for (const field of html_fields) {
+                    if (field in ticket) {
+                        ticket[field] = Markup(ticket[field]);
+                    }
+                }
+            }
+            return tickets;
+        },
     });
 
     return WebsiteEventSessionBooking;
