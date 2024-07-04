d1b614698ddaecfcce2d4343f3e87d87798317c6
Damien <ultrarushgame@gmail.com>
2024-07-04 10:36:05 +0200

    [FIX] bug event_portal_ticket to call website_event

--- event_portal_ticket/__manifest__.py
+++ event_portal_ticket/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '15.0.1.0.0',
     'summary': 'List of all event tickets',
     'description': "List of all event tickets",
-    'depends': ['event', 'portal', 'event_registration_qr_code'],
+    'depends': ['event', 'portal', 'event_registration_qr_code', 'website_event'],
     'data': [
         'security/ir.model.access.csv',
         'security/event_portal_ticket_report.xml',
