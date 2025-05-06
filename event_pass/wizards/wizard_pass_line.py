25c8fbae387c579806acad815e480efb4af07706
Romain <73525560+Rom10811@users.noreply.github.com>
2025-05-06 13:07:24 +0200

    Update wizard_pass_line.py

--- event_pass/wizards/wizard_pass_line.py
+++ event_pass/wizards/wizard_pass_line.py
@@ -42,8 +42,7 @@ class WizardPassLine(models.TransientModel):
         return res
 
     def _generate_code_line_pass(self):
-        # Generate a unique QR code for each pass line
-        return str(uuid4())
+        return '051' + str(uuid4().int)[:10]
 
     def _create_pass_lines(self):
         self.ensure_one()
