0495f3264ed2001cbd0e807cf8cd5ca57f82fbd4
odoo <odoo@localhost>
2026-05-27 23:09:35 +0200

    fix(event_ticket_custom): affiche le nom brut du billet sans [societe] multi-company
    - report_templates.xml : t-field sur attendee.event_ticket_id.name au lieu de event_ticket_id (bypasse name_get du module event_multi_company)
    - portal_ticket_templates.xml : nouveau fichier, surcharge les templates event_portal_ticket et event_session_portal_ticket
    - __manifest__.py : ajout des dependances event_portal_ticket et event_session_portal_ticket

--- event_ticket_custom/__manifest__.py
+++ event_ticket_custom/__manifest__.py
@@ -5,13 +5,18 @@
     "author": "Moka",
     "website": "https://www.moka.cloud",
     "license": "AGPL-3",
-    "website": "https://github.com/OCA/event",
-    "license": "AGPL-3",
     "category": "Event",
-    "depends": ["event", "web_widget_numeric_step", "event_registration_qr_code"],
+    "depends": [
+        "event",
+        "web_widget_numeric_step",
+        "event_registration_qr_code",
+        "event_portal_ticket",
+        "event_session_portal_ticket",
+    ],
     "data": [
         "reports/report_templates.xml",
         "views/event_event.xml",
         "views/event_type.xml",
+        "views/portal_ticket_templates.xml",
     ],
 }
